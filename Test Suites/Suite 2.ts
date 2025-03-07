09fe96089212939e43c89b6202fc8648ddaa5010
ramachandrabalam <rcbalam@gmail.com>
2025-03-07 23:46:07 +0530

    de

--- Test Suites/Suite 2.ts
+++ Test Suites/Suite 2.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>bac23941-f5bd-4731-a9e2-1d05ace299c0</testSuiteGuid>
+   <testCaseLink>
+      <guid>0071bd2a-c32e-4531-bd2b-27839ca80116</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/verify products</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
--- Test Suites/Suite3.ts
+++ Test Suites/Suite3.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Suite 1</name>
+   <name>Suite3</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,26 +10,40 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>99b0fbf9-0928-418d-b880-75bd26177572</testSuiteGuid>
+   <testSuiteGuid>8af22051-ae46-4d3e-bb5d-4256af8b9449</testSuiteGuid>
    <testCaseLink>
-      <guid>fbc99171-d3fc-4745-8fd7-918a6dcf71bc</guid>
+      <guid>823d1b14-db7b-454a-a7b5-6e2d3dc61993</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Open website</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9449991d-dec4-46ea-bf26-2ba1b8e7458c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Verify API testing</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>1d745b82-209a-481f-90d6-5b4002b2e6b2</guid>
+      <guid>2af538f8-7f9f-48fe-ae5f-0043f9c51698</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Verify Card available</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>47ea4c2c-8aea-4f79-b2c4-998e5721c98e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Verify Contact Us</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>ac5f66e6-23d4-4073-bdfb-9dece7a74a73</guid>
+      <guid>12c547b0-b73e-48e5-95ea-08bd81c11f48</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Verify Card available</testCaseId>
+      <testCaseId>Test Cases/verify products</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
